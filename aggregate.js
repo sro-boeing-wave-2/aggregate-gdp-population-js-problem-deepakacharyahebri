e06e90ff9aca0cc2491c519bf6772525673e07e1
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-07-11 21:25:47 +0530

    codes updated

--- aggregate.js
+++ aggregate.js
@@ -2,6 +2,67 @@
  * Aggregates GDP and Population Data by Continents
  * @param {*} filePath
  */
-const aggregate = filePath => {};
+const fs = require('fs');
+
+const gdpMap = new Map();
+const popMap = new Map();
+const getCountryContinentMapping = (filePath) => {
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  const splitString = fileContents.split('\n');
+  let splitByComma;
+  const countryContinentMap = new Map();
+  for (let i = 0; i < splitString.length; i += 1) {
+    splitByComma = splitString[i].split(',');
+    countryContinentMap.set(splitByComma[0], splitByComma[1]);
+  }
+  return countryContinentMap;
+};
+const getCountryPopulationGdpMapping = (filePath, countryContinentMap) => {
+  const mapCountryPopulationGdp = new Map();
+  const csvContents = fs.readFileSync(filePath, 'utf8');
+  const splitString = csvContents.split('\n');
+  let splitByComma;
+  let country;
+  let population;
+  let gdp;
+  let continent;
+  for (let ind = 1; ind < splitString.length - 1; ind += 1) {
+    splitByComma = splitString[ind].split(',');
+    if (splitByComma[0].slice(1, -1) !== 'European Union') {
+      country = splitByComma[0].slice(1, -1);
+      population = splitByComma[4].slice(1, -1);
+      gdp = splitByComma[7].slice(1, -1);
+      continent = countryContinentMap.get(country);
+      mapCountryPopulationGdp.set(country, [population, gdp, continent]);
+    }
+  }
+  return mapCountryPopulationGdp;
+};
+const aggregatePopulationGdp = (value) => {
+  if (popMap.has(value[2])) {
+    popMap.set(value[2], parseFloat(popMap.get(value[2])) + parseFloat(value[0]));
+  } else {
+    popMap.set(value[2], parseFloat(value[0]));
+  }
+  if (gdpMap.has(value[2])) {
+    gdpMap.set(value[2], parseFloat(gdpMap.get(value[2])) + parseFloat(value[1]));
+  } else {
+    gdpMap.set(value[2], parseFloat(value[1]));
+  }
+};
+const aggregate = (filePath) => {
+  const outputfile = './output/output.json';
+  const countryContinentMap = getCountryContinentMapping('./cc-mapping.txt');
+  const mapCountryPopulationGdp = getCountryPopulationGdpMapping(filePath, countryContinentMap);
+  mapCountryPopulationGdp.forEach(aggregatePopulationGdp);
+  const toJasonFormat = {};
+  gdpMap.forEach((value, key) => {
+    toJasonFormat[key] = {
+      GDP_2012: value,
+      POPULATION_2012: popMap.get(key),
+    };
+  });
+  fs.writeFileSync(outputfile, JSON.stringify(toJasonFormat));
+};
 
 module.exports = aggregate;
