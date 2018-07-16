80c3ed8efa7257b361f09be38dddc28f15c2603a
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-07-16 19:08:01 +0530

    code enhanced

--- aggregate.js
+++ aggregate.js
@@ -10,64 +10,35 @@ const readFileAsync = path => new Promise((resolve, reject) => {
     else resolve(data);
   });
 });
-const writeFileAsync = (path, data) => new Promise((resolve, reject) => {
-  fs.writeFile(path, data, (err) => {
-    if (!err) resolve(data);
-    else reject(err);
-  });
-});
-const getCountryContinentMap = (fileContents) => {
-  const countryContinentMap = new Map();
-  const getRows = fileContents.split('\n');
-  let splitByComma;
-  for (let i = 0; i < getRows.length - 1; i += 1) {
-    splitByComma = getRows[i].split(',');
-    countryContinentMap.set(splitByComma[0], splitByComma[1]);
-  }
-  return countryContinentMap;
-};
-const getCSVContentsAsObjects = (fileContents, countryContinentMap) => {
-  const rowOfContents = fileContents.replace(/["']+/g, '').split('\n');
-  const headers = rowOfContents[0].split(',');
-  const objectArray = [];
-  let obj;
-  for (let i = 1; i < rowOfContents.length - 1; i += 1) {
-    obj = {};
-    const splitByComma = rowOfContents[i].split(',');
-    if (splitByComma[0] !== 'European Union') {
-      for (let j = 0; j < headers.length; j += 1) {
-        if (j === 0) obj[headers[j]] = splitByComma[j];
-        else obj[headers[j]] = parseFloat(splitByComma[j]);
-      }
-      obj.Continent = countryContinentMap.get(splitByComma[0]);
-      objectArray.push(obj);
-    }
-  }
-  return objectArray;
-};
-const aggregate = filePath => new Promise((resolve, reject) => {
-  Promise.all([readFileAsync(filePath), readFileAsync('./cc-mapping.txt')]).then((values) => {
-    const countryMapData = values[1];
-    const csvData = values[0];
-    const countryContinentMap = getCountryContinentMap(countryMapData);
-    const objectArray = getCSVContentsAsObjects(csvData, countryContinentMap);
-    const toJSONObject = {};
-    objectArray.forEach((obj) => {
-      try {
-        toJSONObject[obj.Continent].GDP_2012 += obj['GDP Billions (US Dollar) - 2012'];
-        toJSONObject[obj.Continent].POPULATION_2012 += obj['Population (Millions) - 2012'];
-      } catch (e) {
-        toJSONObject[obj.Continent] = {
-          GDP_2012: obj['GDP Billions (US Dollar) - 2012'],
-          POPULATION_2012: obj['Population (Millions) - 2012'],
-        };
+const aggregate = filePath => new Promise((resolve) => {
+  Promise.all([readFileAsync(filePath), readFileAsync('country-continent-map.json')]).then((values) => {
+    const mapper = JSON.parse(values[1]);
+    const csvFileContents = values[0].replace(/["']/g, '').split('\n');
+    const headers = csvFileContents[0].split(',');
+    const rowsOfCountryData = csvFileContents.slice(1, csvFileContents.length);
+    const countryIndex = headers.indexOf('Country Name');
+    const gdpIndex = headers.indexOf('GDP Billions (US Dollar) - 2012');
+    const populationIndex = headers.indexOf('Population (Millions) - 2012');
+    const aggregatedGdpPopulation = {};
+    rowsOfCountryData.forEach((row) => {
+      const splitData = row.split(',');
+      if (mapper[splitData[countryIndex]] !== undefined) {
+        try {
+          aggregatedGdpPopulation[mapper[splitData[countryIndex]]].GDP_2012
+          += parseFloat(splitData[gdpIndex]);
+          aggregatedGdpPopulation[mapper[splitData[countryIndex]]].POPULATION_2012
+          += parseFloat(splitData[populationIndex]);
+        } catch (e) {
+          aggregatedGdpPopulation[mapper[splitData[countryIndex]]] = {
+            GDP_2012: parseFloat(splitData[gdpIndex]),
+            POPULATION_2012: parseFloat(splitData[populationIndex]),
+          };
+        }
       }
     });
-    const dataToBeWrittenToFile = JSON.stringify(toJSONObject, 2, 2);
-    writeFileAsync('./output/output.json', dataToBeWrittenToFile).then((data) => {
-      resolve(data);
-    }, err => reject(err));
+    fs.writeFile('./output/output.json', JSON.stringify(aggregatedGdpPopulation), (err) => {
+      if (!err) resolve();
+    });
   }, () => { });
 });
-
 module.exports = aggregate;
